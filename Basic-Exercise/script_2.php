8d31fce64254b6879f3a5cee3c98ddece39855da
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-09-17 13:39:53 +0530

    My name is John, and I am a PHP developer.

---
+++ Basic-Exercise/script_2.php
@@ -0,0 +1,6 @@
+/* Create a PHP program that uses concatenation to display the following message: My name is John, and I am a PHP developer. */
+<?php
+
+echo"My name is John, and I am a PHP Developer";
+
+?>
