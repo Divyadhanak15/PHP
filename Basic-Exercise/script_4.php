202ce5dccba9427df674369ce995a78130c5acb9
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-09-17 13:44:14 +0530

    Hello, John Doe!

---
+++ Basic-Exercise/script_4.php
@@ -0,0 +1,9 @@
+/* Write a PHP program that assigns your first name to the variable $firstName and your last name to the variable $lastName. Use concatenation to display a greeting message like "Hello, John Doe!". */
+<?php
+
+$firstname = "divya";
+$lastName= "Dhanak";
+
+echo "Hello $firstname $lastName";
+
+?>
