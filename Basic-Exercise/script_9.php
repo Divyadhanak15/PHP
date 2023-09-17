f66c48eb6e8f6a1c39feb86a336edc9f81a833ba
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-09-17 13:53:41 +0530

    strlen() function

---
+++ Basic-Exercise/script_9.php
@@ -0,0 +1,8 @@
+/* Write a PHP program that assigns a string to the variable $text and uses the strlen() function to display its length along with the original string. */
+<?php
+
+$text = "Divya";
+
+echo strlen($text);
+
+?>
