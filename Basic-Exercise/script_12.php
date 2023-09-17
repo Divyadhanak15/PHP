3ad41dd1544e80903416e2c1fc45f0a3bbee8312
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-09-17 13:58:28 +0530

    Unordered list

---
+++ Basic-Exercise/script_12.php
@@ -0,0 +1,10 @@
+/* Develop a PHP program that uses echo to display an unordered list (<ul>) with three list items (<li>) containing different colors. */
+<?php
+
+echo "<ul>
+ <li><font color = blue>Divya</font></li>
+ <li><font color = blue>Janvi</font></li>
+ <li><font color = blue>Alka</font></li>
+</ul>";
+
+?>
