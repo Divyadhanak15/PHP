e7f9d669085d88ebb8d270b5f990da58595373b6
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-09-17 13:47:58 +0530

    Reassigns it to new value

---
+++ Basic-Exercise/script_6.php
@@ -0,0 +1,9 @@
+/* Develop a PHP program that assigns an initial value to the variable $x, then reassigns it to a new value, and finally echoes the value. */
+<?php 
+
+$x = 5;
+$y = $x;
+
+echo "$y";
+
+?>
