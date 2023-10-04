607d0a12510a915972497ef9ef0b4336157845d9
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-04 14:32:03 +0530

    even or odd

---
+++ Task2/Script_11.php
@@ -0,0 +1,12 @@
+<?php  
+// Create a PHP script that checks whether a given number is even or odd and displays the result.
+$number=2;  
+if($number%2==0)  
+{  
+ echo "The given number is Even Number";   
+}  
+else  
+{  
+ echo "The given number is Odd Number";  
+}   
+?>  
