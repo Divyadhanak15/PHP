2937b0625f383b17a7273187e9e4a2f39a6180a2
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-04 14:33:39 +0530

    converts temperature

---
+++ Task2/Script_13.php
@@ -0,0 +1,10 @@
+<?php  
+//Develop a PHP script that converts temperature from Celsius to Fahrenheit using the formula (C × 9/5) + 32.
+       $celsius = 58 ;  
+	   
+       $fahrenheit = (($celsius*9)/5)+32 ;
+	   
+       echo("Temperature in Fahrenheit is:  ");
+	   
+       echo($fahrenheit);  
+?>  
