c817fad980a941e87b928fcf98919de288ed0e42
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-04 14:34:39 +0530

    sum of all numbers

---
+++ Task2/Script_14.php
@@ -0,0 +1,7 @@
+<?php  
+// Create a PHP script that calculates the sum of all numbers from 1 to 100 and displays the result.
+       $C =20;  
+       $fahrenheit = (($C*9)/5)+32 ;   
+       echo("Temperature in Fahrenheit is :  $fahrenheit  ");  
+       
+?>  
