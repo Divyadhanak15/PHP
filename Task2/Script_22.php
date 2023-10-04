e7674c3d48d43714b25aab11fbf4541c20129e13
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-04 14:39:40 +0530

    area of a circle

---
+++ Task2/Script_22.php
@@ -0,0 +1,11 @@
+<?php
+//Script to calculate the area of a circle based on the given radius and display the result. 
+
+$r=10;
+$pi=3.14;
+
+$a = $pi*$r*$r;
+
+echo "area of circle is: ", $a;
+
+?>
