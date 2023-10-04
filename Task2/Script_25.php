d231f5e078a99d840522c227c3e3c4cbb57a42e2
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-04 14:42:15 +0530

    swap 2 number

---
+++ Task2/Script_25.php
@@ -0,0 +1,13 @@
+<?php  
+// Script to swap 2 nos without using 3rd variable using arithmetic operators.
+$a=20;  
+$b=40;  
+
+//using arithmetic operation  
+$a=$a+$b;  
+$b=$a-$b;  
+$a=$a-$b;  
+
+echo "Value of a: $a</br>";  
+echo "Value of b: $b</br>";  
+?>  
