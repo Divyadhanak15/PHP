cf94cb9140570dd2eea67975f54b33361c5e0c97
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-04 14:32:49 +0530

    concatenate two strings

---
+++ Task2/Script_12.php
@@ -0,0 +1,9 @@
+<?php
+//Write a PHP script to concatenate two strings and display the combined result.
+$text1 = 'Hello';
+$text2 = 'Good Morning';
+$text3 = "$text1"."$text2";
+
+echo "$text3";
+
+?>
