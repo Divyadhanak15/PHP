caacd0717d5c0f9cc2b012b43b0ea62a6abce117
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:34:40 +0530

    count()

---
+++ Task3/Script_27.php
@@ -0,0 +1,6 @@
+<?php
+// Create a PHP script to calculate and display the length of an array using the count() function.
+
+$course=array("PHP","C Lang","DEA", "Maths", "HTML");
+echo count($course);
+?>
