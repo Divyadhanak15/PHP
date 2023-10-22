62a9dbc14974013690d4df9792f2d0b10249633e
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:44:09 +0530

    array_push() & array_pop()

--- Task3/Script_37.php
+++ Task3/Script_37.php
@@ -2,7 +2,6 @@
 // Develop a PHP script that uses the array_push() function to add elements to an array and the array_pop() function to remove elements from the end.
 
 $arr = array(); 
-
 array_push($arr, 1, 2, 3, 4); 
 
 print_r("array_push method </br>"); 
