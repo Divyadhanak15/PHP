05e0837e62e52aec84702bc5b4f490f91152ba59
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:47:16 +0530

    array_values()

---
+++ Task3/Script_44.php
@@ -0,0 +1,6 @@
+<?php
+// Write a PHP script to extract the values from an associative array using the array_values() function.
+
+$a=array("Name"=>"Peter","Age"=>"41","Country"=>"USA");
+print_r(array_values($a));
+?>
