93c6c62abb33ca0a1746b874a7a771700bdb4445
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:48:33 +0530

    array_map()

---
+++ Task3/Script_47.php
@@ -0,0 +1,11 @@
+<?php
+// Write a PHP script that applies a user-defined function to all elements of an array using the array_map() function.
+
+function myfunction($v)
+{
+  return($v*$v);
+}
+
+$a=array(1,2,3,4,5);
+print_r(array_map("myfunction",$a));
+?>
