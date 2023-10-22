b7c586a4e3e8ad3f6d7bf5f3dec965270880043e
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:50:41 +0530

    array_merge()

---
+++ Task3/Script_52.php
@@ -0,0 +1,7 @@
+<?php
+// Develop a PHP script to merge two associative arrays using the array_merge() function and display the combined result.
+
+$a1=array("a"=>"red","b"=>"green");
+$a2=array("c"=>"blue","d"=>"yellow");
+print_r(array_merge($a1,$a2));
+?>
