5a4773f71863cd3980baf6c3d91d01acf0032f46
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:49:24 +0530

    array_chunk()

---
+++ Task3/Script_49.php
@@ -0,0 +1,8 @@
+<?php 
+// Develop a PHP script to split an array into chunks of a specified size using the array_chunk() function.
+
+$input_array = array('a', 'b', 'c', 'd', 'e'); 
+
+print_r(array_chunk($input_array, 2)); 
+
+?> 
