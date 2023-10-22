b436c12d0247df6de18ff03593c438fb78787bef
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:46:47 +0530

    array_keys()

---
+++ Task3/Script_43.php
@@ -0,0 +1,8 @@
+<?php
+// Develop a PHP script to extract the keys from an associative array using the array_keys() function.
+
+$numbers = [10, 20, 30, 40];
+
+$keys = array_keys($numbers);
+
+print_r($keys);
