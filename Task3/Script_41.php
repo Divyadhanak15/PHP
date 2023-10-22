0d8897465d429e13b11606f117788ad0aab486f2
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:45:46 +0530

    array_intersect()

---
+++ Task3/Script_41.php
@@ -0,0 +1,15 @@
+<?php 
+// Write a PHP script that finds the intersection of two arrays using the array_intersect() function.
+
+function Intersect($array1, $array2, $array3) 
+{ 
+	$result = array_intersect($array1, $array2, $array3); 
+	return($result); 
+} 
+
+$array1 = array(5, 10, 15, 20, 25, 30); 
+$array2 = array(20, 10, 15, 55, 100, 110, 30); 
+$array3 = array(10, 15, 30, 55, 100, 95); 
+print_r(Intersect($array1, $array2, $array3)); 
+
+?> 
