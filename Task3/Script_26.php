2a7e46ba1704a6e4ac826533b6b973778a21951d
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:33:34 +0530

    rand()

---
+++ Task3/Script_26.php
@@ -0,0 +1,10 @@
+<?php 
+// Write a PHP script that generates a random number between a specified range using the rand() function and displays it.
+
+$num1 = rand(); 
+
+$num2 = rand(7, 100); 
+echo "Random number in range: ", $num2;
+
+?> 
+
