09ef416d615a3fde60f1e105cccf235fc4665047
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:50:13 +0530

    array_push()

---
+++ Task3/Script_51.php
@@ -0,0 +1,8 @@
+<?php
+// Create a PHP script that adds elements to an associative array using the array_push() function.
+
+$fruits = array("orange", "banana");
+array_push($fruits, "apple", "raspberry", "pineapple", "strawberry");
+
+print_r($fruits);
+?>
