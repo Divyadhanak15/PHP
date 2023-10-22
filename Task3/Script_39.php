401c5c66e284d8b49079ebbb80396c436a4782d8
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:44:44 +0530

    array_slice()

---
+++ Task3/Script_39.php
@@ -0,0 +1,8 @@
+<?php 
+// Create a PHP script that extracts a portion of an array using the array_slice() function and displays the result.
+
+$array = array("php", "java", "css", "python", "html"); 
+												 
+print_r(array_slice($array, 1, 2, true)); 
+
+?> 
