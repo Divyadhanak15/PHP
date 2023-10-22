79787399599e12ee61e1ecd3989fb04d25307c6b
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:39:58 +0530

    shuffle()

---
+++ Task3/Script_35.php
@@ -0,0 +1,9 @@
+<?php
+// Write a PHP script to shuffle the elements of an array using the shuffle() function.
+
+$my_array = array("red","green","blue","yellow","purple");
+print_r($my_array);
+echo"<br/>";
+shuffle($my_array);
+print_r($my_array);
+?>
