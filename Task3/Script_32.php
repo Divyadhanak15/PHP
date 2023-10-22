a5ebc554a7b7ac93339dcc1d41adf344312d7999
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:38:13 +0530

    array_merge()

---
+++ Task3/Script_32.php
@@ -0,0 +1,9 @@
+<?php
+// Write a PHP script to merge two arrays using the array_merge() function and display the combined result.
+
+$a1=array("Helloo","Everyone");
+$a2=array("Good","Morning"); 
+
+print_r(array_merge($a1,$a2));
+
+?>
