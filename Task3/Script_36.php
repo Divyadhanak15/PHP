3121a33c995f925d83f97b010a011ed7ffed02c8
Divya <143484447+Divyadhanak15@users.noreply.github.com>
2023-10-22 17:40:31 +0530

    array_sum()

---
+++ Task3/Script_36.php
@@ -0,0 +1,9 @@
+<?php 
+// Create a PHP script that calculates and displays the sum of all elements in an array using the array_sum() function.
+
+//array whose sum is to be calculated 
+$a = array(12, 24, 3, 48, 50, 90); 
+
+//calculating sum 
+print_r(array_sum($a)); 
+?> 
